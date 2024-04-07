3448f408426349378af37ed0ea011f616364d744
RavinMehta <rravin3211@gmail.com>
2024-04-07 19:51:32 +0530

    commiting project in git repo

---
+++ AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.java.practice;
+import java.util.*;
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println("sum of two numbers a and b is: "+sum);
+ 
+	}
+
+}
